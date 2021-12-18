b4368cd047f5603f6dd3f0d3e28759a3eda6daf6
Aman Singh <aman.singh@powerschool.com>
2021-12-18 23:15:41 +0530

    Implemented hinge loss single, hinge loss full, perceptron_single, perceptron, and perceptron_average

--- sentiment_analysis/project1.py
+++ sentiment_analysis/project1.py
@@ -33,7 +33,13 @@ def hinge_loss_single(feature_vector, label, theta, theta_0):
     Returns: A real number representing the hinge loss associated with the
     given data point and parameters.
     """
-    # Your code here
+    z = label * (np.dot(theta, feature_vector) + theta_0)
+    # z = label * (theta @ feature_vector + theta_0)
+    if z >= 1:
+        return 0
+    else:
+        return 1 - z
+    # return max(0, 1 - z)
     raise NotImplementedError
 
 
@@ -55,7 +61,14 @@ def hinge_loss_full(feature_matrix, labels, theta, theta_0):
     given dataset and parameters. This number should be the average hinge
     loss across all of the points in the feature matrix.
     """
-    # Your code here
+    total_hinge_loss = 0
+    n = len(labels)
+    for i in range(n):
+        total_hinge_loss += hinge_loss_single(feature_matrix[i], labels[i], theta, theta_0)
+    return total_hinge_loss/n
+    # ys = feature_matrix @ theta + theta_0
+    # loss = np.maximum(1 - ys * labels, np.zeros(len(labels)))
+    # return np.mean(loss)
     raise NotImplementedError
 
 
@@ -81,7 +94,10 @@ def perceptron_single_step_update(
     real valued number with the value of theta_0 after the current updated has
     completed.
     """
-    # Your code here
+    if label * (feature_vector @ current_theta + current_theta_0) <= 0:
+        current_theta += label * feature_vector
+        current_theta_0 += label
+    return (current_theta, current_theta_0)
     raise NotImplementedError
 
 
@@ -110,11 +126,11 @@ def perceptron(feature_matrix, labels, T):
     theta_0, the offset classification parameter, after T iterations through
     the feature matrix.
     """
-    # Your code here
+    current_theta, current_theta_0 = np.zeros(len(feature_matrix[0])), 0
     for t in range(T):
         for i in get_order(feature_matrix.shape[0]):
-            # Your code here
-            pass
+            current_theta, current_theta_0 = perceptron_single_step_update(feature_matrix[i], labels[i], current_theta, current_theta_0)
+    return (current_theta, current_theta_0)
     raise NotImplementedError
 
 
@@ -147,7 +163,14 @@ def average_perceptron(feature_matrix, labels, T):
     Hint: It is difficult to keep a running average; however, it is simple to
     find a sum and divide.
     """
-    # Your code here
+    theta, theta_sum, theta_0, theta_0_sum, counter = np.zeros(len(feature_matrix[0])), np.zeros(len(feature_matrix[0])), 0,  0, 0
+    for t in range(T):
+        for i in get_order(feature_matrix.shape[0]):
+            theta, theta_0 = perceptron_single_step_update(feature_matrix[i], labels[i], theta, theta_0)
+            theta_sum += theta
+            theta_0_sum += theta_0
+            counter += 1
+    return (theta_sum/counter, theta_0_sum/counter)
     raise NotImplementedError
 
 
